b33991578b717c3a23a591ff949e7b476b87c080
Zach Parks <zach@alliware.com>
2024-10-02 10:06:00 -0500

    Breaking things... again.

--- src/rules.ts
+++ src/rules.ts
@@ -1,100 +1,176 @@
-/**
- * Creates a PopTracker-compatible rules array.
- * @remarks OR (`|`) has higher priority than AND (`&`). Parens are not supported (yet).
- * @param expressions
- */
-export function rule(expressions: string): string[] {
-    const orExpressions = expressions.split("|").map((expr) => expr.trim());
-    const allRules: string[] = [];
+import dedent from "dedent";
 
-    for (const orExpression of orExpressions) {
-        const rules: string[] = [];
-        const andExpressions = orExpression.split("&").map((expr) => expr.trim());
-        for (const andExpression of andExpressions) {
-            switch (andExpression) {
-                // Strict Accessibility Modifiers
-                case "MOVELESS":
-                    rules.push("^$MoveAccessibility");
-                    break;
-                case "CAPLESS":
-                    rules.push("^$CapsAccessibility");
-                    break;
-                case "CANNLESS":
-                    rules.push("^$CannAccessibility");
-                    break;
+type RuleToken = { type: AllowedTokenTypes, index: number, value?: string | number };
+type TokenMatcher = { expr: RegExp, type: AllowedTokenTypes | null, extract?: (match: string) => string };
+type AllowedTokenTypes =
+    | "OR"
+    | "MOVE_GROUP"
+    | "CANNON"
+    | "STARS"
+    | "ACCESS"
+    | "NONSTRICT"
+    | "NO_ER"
+    | "COMPLETED_SUB"
+    | "BEATEN_BOWSER2"
+    | "LITERAL"
+    | "EOL";
 
-                // Has Caps
-                case "VC":
-                case "MC":
-                case "WC":
-                    rules.push(`$HasCap|${andExpression}`);
-                    break;
+const acceptedTokens: TokenMatcher[] = [
+    { expr: /\s+/, type: null },
+    { expr: /\|/, type: "OR" },
+    {
+        expr: /(WC|MC|VC|TJ|LJ|BF|SF|WK|DV|GP|KI|CL|LG)(\/(WC|MC|VC|TJ|LJ|BF|SF|WK|DV|GP|KI|CL|LG))*/,
+        type: "MOVE_GROUP",
+        extract: (match) => match,
+    },
+    { expr: /(MOVELESS|CANNLESS|CAPLESS)/, type: "NONSTRICT", extract: (match) => match },
+    { expr: /CANN/, type: "CANNON" },
+    { expr: /STARS:\w+/, type: "STARS", extract: (match) => match.split(":")[1] },
+    { expr: /NAR/, type: "NO_ER" },
+    { expr: /ACCESS:\w+/, type: "ACCESS", extract: (match) => match.split(":")[1] },
+    { expr: /BOWSER2/, type: "BEATEN_BOWSER2" },
+    { expr: /SUB/, type: "COMPLETED_SUB" },
+    { expr: /\^?\$\w+/, type: "LITERAL", extract: (match) => match },
+];
 
-                // No Area Randomizer
-                case "NAR":
-                    rules.push("$NoAreaRandomizer");
-                    break;
-
-                // Special DDD
-                case "SUB":
-                    rules.push("$HasCompleted|3626056"); // DDD: Bowser's Sub Star
-                    break;
-                case "BEATBOWSER2":
-                    rules.push("$HasCompleted|3626179"); // Second Bowser Defeat
-                    break;
-
-                // HMC Access
-                case "HMC":
-                    rules.push("^$CanAccessHMC");
-                    break;
-
-                // Star/Key Requirements
-                case "BASEMENT":
-                    rules.push("$HasKey|B");
-                    break;
-                case "SECONDFLOOR":
-                    rules.push("$HasKey|U");
-                    break;
-                case "THIRDFLOOR":
-                    rules.push("$HasKey|U,$HasStars|F2");
-                    break;
-                case "BOWSER1":
-                    rules.push("$HasStars|F1");
-                    break;
-                case "BOWSER2":
-                    rules.push("$HasStars|B1");
-                    break;
-                case "BOWSER3":
-                    rules.push("$HasStars|F3");
-                    break;
-                case "MIPS1":
-                    rules.push("$HasStars|MIPS1");
-                    break;
-                case "MIPS2":
-                    rules.push("$HasStars|MIPS2");
-                    break;
-
-                default:
-                    // Star Requirements
-                    if (andExpression.startsWith("STARS:")) {
-                        rules.push(`$HasStars|${andExpression.replace("STARS:", "")}`);
-                        break;
+function* tokenize(input: string): Generator<RuleToken> {
+    let index = 0;
+    while (index < input.length) {
+        let hasMatch = false;
+        for (const { expr, type, extract } of acceptedTokens) {
+            const exprMatcher = new RegExp(expr.source, "y");
+            exprMatcher.lastIndex = index;
+            const matched = exprMatcher.exec(input);
+            if (matched) {
+                index += matched[0].length;
+                if (type !== null) {
+                    const token: RuleToken = { type, index };
+                    if (extract) {
+                        token.value = extract(matched[0]);
                     }
 
-                    // Has Cannon
-                    if (andExpression.startsWith("CANN:")) {
-                        rules.push(`$HasCannon|${andExpression.replace("CANN:", "")}`);
-                        break;
-                    }
+                    yield token;
+                }
 
-                    // Move Requirements
-                    rules.push(`$HasMoves|${andExpression}`);
+                hasMatch = true;
             }
         }
 
-        // Append new rule.
-        allRules.push(rules.join(","));
+        validate(hasMatch, input, index + 1);
     }
 
-    return allRules;
+    yield { type: "EOL", index: input.length };
+}
+
+function validate(result: boolean, input: string, index: number): void {
+    if (result) {
+        return;
+    }
+
+    const message = dedent(`
+        Unexpected token at position: ${index - 1}
+        
+        Rule substring with error:
+        "${input}"
+         ${"^".padStart(index, " ")}
+    `);
+
+    throw new SyntaxError(message);
+}
+
+export function buildRules(input: string, stage: string): string[] {
+    if (input === "") {
+        return [];
+    }
+
+    const rules = new Set<string>();
+    const subrules = new Set<string>();
+    let lastToken: RuleToken | undefined = undefined;
+    for (const token of tokenize(input)) {
+        switch (token.type) {
+            case "OR":
+                validate(!!lastToken && lastToken.type !== "OR", input, token.index);
+                rules.add(Array.from(subrules).sort().join(","));
+                subrules.clear();
+                break;
+
+            case "MOVE_GROUP": {
+                const moves = new Set((token.value as string).split("/")); // Remove duplicates.
+                subrules.add(`$Has|${Array.from(moves).sort().join("|")}`);
+                break;
+            }
+
+            case "CANNON":
+                subrules.add(`$HasCannon|${stage}`);
+                break;
+
+            case "STARS": {
+                const value = token.value as string;
+                switch (value) {
+                    case "F1":
+                    case "B1":
+                    case "F2":
+                    case "F3":
+                    case "MIPS1":
+                    case "MIPS2":
+                        subrules.add(`$HasStars|${value}`);
+                        break;
+
+                    default:
+                        validate(!isNaN(parseInt(value)), input, token.index);
+                        subrules.add(`$HasStars|${value}`);
+                }
+
+                break;
+            }
+
+            case "ACCESS": {
+                const value = token.value as string;
+                switch (value) {
+                    case "B1":
+                    case "F2":
+                    case "F3":
+                    case "HMC":
+                        subrules.add(`$CanAccess|${value}`);
+                        break;
+
+                    default:
+                        // Immediate failure.
+                        validate(false, input, token.index);
+                }
+
+                break;
+            }
+
+            case "NONSTRICT":
+                subrules.add(`^$StrictAccessibility|${token.value}`);
+                break;
+
+            case "NO_ER":
+                subrules.add("$NoAreaRando");
+                break;
+
+            case "COMPLETED_SUB":
+                subrules.add("$Sub");
+                break;
+
+            case "BEATEN_BOWSER2":
+                subrules.add("$BeatBowser2");
+                break;
+
+            case "LITERAL":
+                subrules.add(token.value as string);
+                break;
+
+            case "EOL":
+                validate(!!lastToken && lastToken.type !== "OR", input, token.index);
+                rules.add(Array.from(subrules).sort().join(","));
+                subrules.clear();
+                break;
+        }
+
+        lastToken = token;
+    }
+
+    return Array.from(rules);
 }
